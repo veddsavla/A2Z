e1f82684e3fa15ebe8f2fcf55a5ff7163eaf12ae
Vedansh Savla <savlavedansh@gmail.com>
2025-02-28 08:01:30 +0530

    New commit message

--- Step 3/lec2/5pairWithMaxSum.cpp
+++ Step 3/lec2/5pairWithMaxSum.cpp
@@ -1,21 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
-public:
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
     // Function to find pair with maximum sum
-    int pairWithMaxSum(vector<int> &arr)
-    {
-        int N = arr.size();
+    int pairWithMaxSum(vector<int> &arr) {
         int ans = 0;
-
-        // Iterate over the array
-        for (int i = 1; i < N; i++)
-            // Update ans with maximum sum of current and previous element
-            ans = max(arr[i] + arr[i - 1], ans);
-
-        // Return the maximum sum
+        for(int i = 1; i< arr.size(); i++){
+            ans = max(arr[i]+arr[i-1], ans);
+        }
         return ans;
+
     }
 };
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        cout << ob.pairWithMaxSum(nums) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
---
+++ Step 3/lec2/9nextPermutation.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &nums)
+    {
+        int n = nums.size();
+        int index = -1;
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1)
+        {
+            return reverse(nums.begin(), nums.end());
+        }
+
+        for (int i = n - 1; i >= index; i--)
+        {
+            if (nums[i] > nums[index])
+            {
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+
+        return reverse(nums.begin() + index + 1, nums.end());
+    }
+};
