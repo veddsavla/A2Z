395b5940513fc7c4c7cff22d1325672626e1e0da
veddsavla <savlavedansh@gmail.com>
2025-06-10 21:12:16 +0530

    step 4 lec 1

---
+++ Step 4/BS on 1D Array/9dupNumsBS.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+using std::vector;
+
+
+class Solution
+{
+public:
+    int singleNonDuplicate(vector<int> &nums)
+    {
+
+        int n = nums.size();
+
+        if (nums.size() == 1)
+            return nums[0];
+        if (nums[0] != nums[1])
+            return nums[0];
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
+
+        int low = 1;
+        int high = nums.size() - 2;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+                return nums[mid];
+
+            if (mid % 2 == 1 && nums[mid] == nums[mid - 1] || mid % 2 == 0 && nums[mid] == nums[mid + 1])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
