4577bce99aa4b667f52b940d16c8239a6e0e4574
veddsavla <savlavedansh@gmail.com>
2025-06-14 13:17:08 +0530

    step 4 lec 3

---
+++ Step 4/BS on 2D Array/2searchMatrix2D.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using std::max;
+using std::min;
+using std::vector;
+
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int low = 0, high = n * m - 1;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int row = mid / m, col = mid % m;
+            if (matrix[row][col] == target)
+                return true;
+            else if (matrix[row][col] > target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return false;
+    }
+};
