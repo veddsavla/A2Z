5f2e80875b3fd20452c4d677fee6cbe8ce66e43c
veddsavla <savlavedansh@gmail.com>
2025-06-27 10:58:04 +0530

    step 8 lec 3

---
+++ Step 8: Bit Manipulation/Lec 3/1primeCount.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    int countPrimes(int n)
+    {
+        int count = 0;
+        if (n <= 2)
+            return 0;
+        vector<bool> prime(n, true);
+        prime[0] = prime[1] = false;
+
+        for (int i = 2; i * i < n; i++)
+        {
+            if (prime[i])
+            {
+                for (int j = i * i; j < n; j += i)
+                {
+                    prime[j] = false;
+                }
+            }
+        }
+
+        for (int k = 2; k < n; k++)
+        {
+            if (prime[k])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
